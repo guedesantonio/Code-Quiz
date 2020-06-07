fc15c001236adc244985664fca9fd02ed8d1a98b
Antonio Guedes <gguedesantonio@gmail.com>
2020-06-07 20:33:51 +1000

    Debuging timecountdown function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //add variable with necessary html elements
 var startBtn = document.querySelector("#start-btn");
+var timerEl = document.getElementById("#seconds");
+console.log(timerEl.nodeValue);
 // listener that when the btn start quiz is clicked activate function timerCountdown()
 startBtn.addEventListener("click", function(event) {
 event.preventDefault();
@@ -9,14 +11,24 @@ var element = event.target;
   if (element.matches("button") === true) {
     timerCountdown()
   }})
-// function timerCountdown() start the countdown and call function random question
+// function timerCountdown() start the countdown and call function newquestion
 function timerCountdown() {
-    alert("is working");
+    var timeLeft = "60";
+  
+    var timeInterval = setInterval(function() {
+      timerEl.textContent = timeLeft;
+      timeLeft--;
+  
+      if (timeLeft === 0) {
+        timerEl.textContent = "0";
+        alert("Is working");
+        clearInterval(timeInterval);
+      }
+  
+    }, 1000);
 }
-//     when time is up call function recordscore
-// random question that gives the screen a new question 
+//     when time is up call function recordscore && Question counter call record score
+// newquestion function that gives the screen a new question 
 // function record score records the score on storage 
 // function that when Highscore page is loaded adds whatever is on storage to Highscores
-// listener that when the btn goback is clicked goes back to start quiz page
 // listener that when the btn clear highscore is clicked clear Highscores
-
