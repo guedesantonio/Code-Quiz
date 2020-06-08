22ea52ff33f256a8dcbaa18a83a12303b3f0fff3
Antonio Guedes <gguedesantonio@gmail.com>
2020-06-08 15:33:17 +1000

    Timer working

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 //add variable with necessary html elements
 var startBtn = document.querySelector("#start-btn");
-var timerEl = document.getElementById("#seconds");
-console.log(timerEl.nodeValue);
+var timerEl = document.getElementById("seconds");
+var timeInterval;
+
 // listener that when the btn start quiz is clicked activate function timerCountdown()
-startBtn.addEventListener("click", function(event) {
-event.preventDefault();
-var element = event.target;
+startBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  var element = event.target;
 
   // If that element is a button...
   if (element.matches("button") === true) {
     timerCountdown()
-  }})
+  }
+})
 // function timerCountdown() start the countdown and call function newquestion
 function timerCountdown() {
-    var timeLeft = "60";
-  
-    var timeInterval = setInterval(function() {
-      timerEl.textContent = timeLeft;
-      timeLeft--;
-  
-      if (timeLeft === 0) {
-        timerEl.textContent = "0";
-        alert("Is working");
-        clearInterval(timeInterval);
-      }
-  
-    }, 1000);
+  var timeLeft = 60;
+
+  timeInterval = setInterval(function () {
+    timerEl.textContent = timeLeft;
+    timeLeft--;
+
+    if (timeLeft < 1) {
+      timerEl.textContent = 0;
+      alert("Is working");
+      clearInterval(timeInterval);
+    }
+
+  }, 1000);
 }
 //     when time is up call function recordscore && Question counter call record score
 // newquestion function that gives the screen a new question 
