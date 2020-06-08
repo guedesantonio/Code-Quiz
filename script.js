79995cf6aa935cbecf775cf9c9a6feff544106b9
Antonio Guedes <gguedesantonio@gmail.com>
2020-06-08 20:42:02 +1000

    Function newQuestion created

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //add variable with necessary html elements
 var startBtn = document.querySelector("#start-btn");
 var timerEl = document.getElementById("seconds");
+var startEl = document.getElementById("start");
+var q1El = document.getElementById("q1");
+var q2El = document.getElementById("q2");
+var q3El = document.getElementById("q3");
+var q4El = document.getElementById("q4");
+var q5El = document.getElementById("q5");
+var scoreEl = document.getElementById("score");
+var questionsArray = [q1El , q2El , q3El , q4El, q5El]
 var timeInterval;
 var timeLeft = 150;
 
@@ -20,17 +28,40 @@ function timerCountdown() {
   timeInterval = setInterval(function () {
     timerEl.textContent = timeLeft;
     timeLeft--;
+    newQuestion()
 
     if (timeLeft < 1) {
       timerEl.textContent = 0;
-      alert("Is working");
       clearInterval(timeInterval);
     }
 
   }, 1000);
 }
-//     when time is up call function recordscore && Question counter call record score
+    // when time is up call function recordscore && Question counter call record score
+
 // newquestion function that gives the screen a new question 
+function newQuestion() {
+
+  startEl.classList.add('d-none');
+  q1El.classList.remove('d-none');
+
+  // for (i=0,i<questionsArray.length,i++) {
+  //   var currentQuestion = questionsArray[i];
+  //   if (i=0) {
+  //   startEl.classList.add('d-none');
+  //   currentQuestion.classList.remove('d-none');
+  //   }
+  //   else {
+  //   lastQuestion = questionsArray[i-1]  
+  //     lastQuestion.classList.add('d-none');
+  //     currentQuestion.classList.remove('d-none'); 
+  //   }
+    
+  // }
+
+ 
+
+}
 // function record score records the score on storage 
 // function that when Highscore page is loaded adds whatever is on storage to Highscores
 // listener that when the btn clear highscore is clicked clear Highscores
